33a7fd57886043de719ff7330f10093028748963
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-03-27 15:56:48 -0300

    Add files via upload

---
+++ resources/js/fx.js
@@ -0,0 +1,16 @@
+// Intro sequence swap FX
+let welcome = document.getElementsByClassName('intro')[0];
+
+// Changes the word WELCOME to ENTER when hovered.
+function mouseOn() {
+    welcome.innerHTML = 'ENTER';
+}
+welcome.onmouseover = mouseOn;
+
+// Changes the word back to WELCOME after hovering.
+function mouseOut() {
+    welcome.innerHTML = 'WELCOME';
+}
+welcome.onmouseout = mouseOut;
+
+// -- -- -- -- //
